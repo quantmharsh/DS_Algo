cfd1952ee1ba390b7c7488c95932d3c1a28fc44e
Harsh Srivastava <srivastavah240@gmail.com>
2025-09-20 20:22:17 +0530

    (Feat):Added leetcode 3508  Implement Router

---
+++ Design/implementRouter.cpp
@@ -0,0 +1,165 @@
+//3508. Implement Router
+// Design a data structure that can efficiently manage data packets in a network router. Each data packet consists of the following attributes:
+
+// source: A unique identifier for the machine that generated the packet.
+// destination: A unique identifier for the target machine.
+// timestamp: The time at which the packet arrived at the router.
+// Implement the Router class:
+
+// Router(int memoryLimit): Initializes the Router object with a fixed memory limit.
+
+// memoryLimit is the maximum number of packets the router can store at any given time.
+// If adding a new packet would exceed this limit, the oldest packet must be removed to free up space.
+// bool addPacket(int source, int destination, int timestamp): Adds a packet with the given attributes to the router.
+
+// A packet is considered a duplicate if another packet with the same source, destination, and timestamp already exists in the router.
+// Return true if the packet is successfully added (i.e., it is not a duplicate); otherwise return false.
+// int[] forwardPacket(): Forwards the next packet in FIFO (First In First Out) order.
+
+// Remove the packet from storage.
+// Return the packet as an array [source, destination, timestamp].
+// If there are no packets to forward, return an empty array.
+// int getCount(int destination, int startTime, int endTime):
+
+// Returns the number of packets currently stored in the router (i.e., not yet forwarded) that have the specified destination and have timestamps in the inclusive range [startTime, endTime].
+// Note that queries for addPacket will be made in increasing order of timestamp.
+
+ 
+
+// Example 1:
+
+// Input:
+// ["Router", "addPacket", "addPacket", "addPacket", "addPacket", "addPacket", "forwardPacket", "addPacket", "getCount"]
+// [[3], [1, 4, 90], [2, 5, 90], [1, 4, 90], [3, 5, 95], [4, 5, 105], [], [5, 2, 110], [5, 100, 110]]
+
+// Output:
+// [null, true, true, false, true, true, [2, 5, 90], true, 1]
+
+// Explanation
+
+// Router router = new Router(3); // Initialize Router with memoryLimit of 3.
+// router.addPacket(1, 4, 90); // Packet is added. Return True.
+// router.addPacket(2, 5, 90); // Packet is added. Return True.
+// router.addPacket(1, 4, 90); // This is a duplicate packet. Return False.
+// router.addPacket(3, 5, 95); // Packet is added. Return True
+// router.addPacket(4, 5, 105); // Packet is added, [1, 4, 90] is removed as number of packets exceeds memoryLimit. Return True.
+// router.forwardPacket(); // Return [2, 5, 90] and remove it from router.
+// router.addPacket(5, 2, 110); // Packet is added. Return True.
+// router.getCount(5, 100, 110); // The only packet with destination 5 and timestamp in the inclusive range [100, 110] is [4, 5, 105]. Return 1.
+// Example 2:
+
+// Input:
+// ["Router", "addPacket", "forwardPacket", "forwardPacket"]
+// [[2], [7, 4, 90], [], []]
+
+// Output:
+// [null, true, [7, 4, 90], []]
+
+// Explanation
+
+// Router router = new Router(2); // Initialize Router with memoryLimit of 2.
+// router.addPacket(7, 4, 90); // Return True.
+// router.forwardPacket(); // Return [7, 4, 90].
+// router.forwardPacket(); // There are no packets left, return [].
+ 
+
+// Constraints:
+
+// 2 <= memoryLimit <= 105
+// 1 <= source, destination <= 2 * 105
+// 1 <= timestamp <= 109
+// 1 <= startTime <= endTime <= 109
+// At most 105 calls will be made to addPacket, forwardPacket, and getCount methods altogether.
+// queries for addPacket will be made in increasing order of timestamp.
+
+
+
+class Router {
+public:
+    int max_memory;
+    queue<string> q;
+    //map to store data packet all info on basis of key
+    unordered_map<string , vector<int>> packetData;
+    //map to store all data packets timestamp for particular destination
+    unordered_map<int , vector<int>> destiToTimestamp;
+
+    string createKey(int source , int destination , int timestamp)
+    {
+        string key= to_string(source)+"*"+to_string(destination)+"*"+to_string(timestamp);
+        return key;
+    }
+
+    Router(int memoryLimit) {
+        max_memory=memoryLimit;
+        
+    }
+    
+    bool addPacket(int source, int destination, int timestamp) {
+        //create key 
+      string key=  createKey(source , destination , timestamp);
+      //check if we have data packet with same key
+      //already exists
+      if(packetData.find(key) != packetData.end())
+      {
+        return false;
+      }
+      //Before Storing check limit and free up space
+      if(packetData.size() >=max_memory)
+      {
+        forwardPacket();
+      }
+      //strore in map
+      packetData[key]={source  ,destination ,timestamp};
+      //stor in destinationto timestamp map 
+      destiToTimestamp[destination].push_back(timestamp);
+      //push daata into  queue
+      q.push(key);
+      return true;
+
+
+        
+    }
+    
+    vector<int> forwardPacket() {
+        if(packetData.empty())
+        {
+            return {};
+        }
+        string key= q.front();
+        q.pop();
+         vector<int> packet=packetData[key];
+        //remove that packet
+        packetData.erase(key);
+        //also remove  latest timestamp for that particular destinationpacket
+       
+        int desti= packet[1];
+        destiToTimestamp[desti].erase(destiToTimestamp[desti].begin() );
+       
+        return packet;
+
+        
+            }
+    
+    int getCount(int destination, int startTime, int endTime) {
+        //find all timestamp for  pakcets that are having this destination 
+        auto it =destiToTimestamp.find(destination);
+        //Important Condition 
+         if (it == destiToTimestamp.end() || it->second.empty())
+            return 0;
+        //get its value
+       //NOTE: Then copy in another vector it will throw Memory Limit Exceeded  // vector<int> timestamp=it->second;
+        //find lower bound. doing -begin(timestamp) because lower bound returns iterator value but we need its index.
+        int lowerBound =lower_bound(begin(it->second) , end(it->second), startTime )- begin(it->second);
+        int upperBound = upper_bound(begin(it->second) ,end(it->second) , endTime)- begin(it->second);
+        return upperBound-lowerBound;
+            
+    }
+};
+
+/**
+ * Your Router object will be instantiated and called as such:
+ * Router* obj = new Router(memoryLimit);
+ * bool param_1 = obj->addPacket(source,destination,timestamp);
+ * vector<int> param_2 = obj->forwardPacket();
+ * int param_3 = obj->getCount(destination,startTime,endTime);
+ */
