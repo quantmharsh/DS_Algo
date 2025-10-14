40d8a4083e778cfe697596e5394efc8d10819d77
Harsh Srivastava <srivastavah240@gmail.com>
2025-10-14 20:28:19 +0530

    (Feat):Added Leetcode :1352. Product of the Last K Numbers

--- Arrays/Products_Of_Last_K_Numbers.cpp
+++ Arrays/Products_Of_Last_K_Numbers.cpp
@@ -98,6 +98,8 @@ public:
 /**
  * Your ProductOfNumbers object will be instantiated and called as such:
  * ProductOfNumbers* obj = new ProductOfNumbers();
+ * 
+ * 
  * obj->add(num);
  * int param_2 = obj->getProduct(k);
  */
