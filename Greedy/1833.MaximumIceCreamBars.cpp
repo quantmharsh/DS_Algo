10523c021f4310c2c9edef421ff79b4698bf3f24
Harsh Srivastava <srivastavah240@gmail.com>
2026-06-21 15:40:57 +0530

    (Feat):Added Leetcode:1833. Maximum Ice Cream Bars

---
+++ Greedy/1833.MaximumIceCreamBars.cpp
@@ -0,0 +1,20 @@
+// 
+class Solution {
+public:
+    int maxIceCream(vector<int>& costs, int coins) {
+        
+        sort(costs.begin() , costs.end());
+        int cnt=0;
+        for(int i =0;i<costs.size();i++)
+        {
+         if(costs[i]<=coins)
+         {
+            coins-=costs[i];
+            cnt++;
+         }
+
+        }
+        return cnt;
+
+    }
+};
